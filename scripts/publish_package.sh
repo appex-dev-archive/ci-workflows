ee9da0f0fafa2f8bf6be9b92041ea0f4ad9a4b40
Adam Davies <addavies@dunelm.com>
2022-08-22 15:10:39 +0100

    Fix bash command

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-read -a params <<< "$1"
-only_version="${params[0]}"
+ls
 
-echo "Configuring git ..."
-git config --global user.email "$GIT_EMAIL"
-git config --global user.name "$GIT_USERNAME"
-git pull
-git checkout main
+# read -a params <<< "$1"
+# only_version="${params[0]}"
 
-echo "Tagging package version ..."
-tag="$(git describe --tags --abbrev=0)"
-npm version "$tag" -m "Release version: %s"
-git push
+# echo "Configuring git ..."
+# git config --global user.email "$GIT_EMAIL"
+# git config --global user.name "$GIT_USERNAME"
+# git pull
+# git checkout main
 
-if [ "$only_version" = "false" ]
-then
-  echo "Publishing package ..."
-  npm publish
-fi
+# echo "Tagging package version ..."
+# tag="$(git describe --tags --abbrev=0)"
+# npm version "$tag" -m "Release version: %s"
+# git push
+
+# if [ "$only_version" = "false" ]
+# then
+#   echo "Publishing package ..."
+#   npm publish
+# fi
