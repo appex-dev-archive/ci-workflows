5c66274054e8c06b89bf25cb4282e442dfa5dce2
Adam Davies <addavies@dunelm.com>
2022-08-22 17:04:38 +0100

    Revert script

--- scripts/publish_package.sh
+++ scripts/publish_package.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-ls
+read -a params <<< "$1"
+only_version="${params[0]}"
 
-# read -a params <<< "$1"
-# only_version="${params[0]}"
+echo "Configuring git ..."
+git config --global user.email "$GIT_EMAIL"
+git config --global user.name "$GIT_USERNAME"
+git pull
+git checkout main
 
-# echo "Configuring git ..."
-# git config --global user.email "$GIT_EMAIL"
-# git config --global user.name "$GIT_USERNAME"
-# git pull
-# git checkout main
+echo "Tagging package version ..."
+tag="$(git describe --tags --abbrev=0)"
+npm version "$tag" -m "Release version: %s"
+git push
 
-# echo "Tagging package version ..."
-# tag="$(git describe --tags --abbrev=0)"
-# npm version "$tag" -m "Release version: %s"
-# git push
-
-# if [ "$only_version" = "false" ]
-# then
-#   echo "Publishing package ..."
-#   npm publish
-# fi
+if [ "$only_version" = "false" ]
+then
+  echo "Publishing package ..."
+  npm publish
+fi
