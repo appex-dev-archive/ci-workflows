01b21df5beda03c8b37ff5f139417b141b2e01fe
Adam Davies <adam.davies@appex-development.com>
2022-09-07 09:04:07 +0100

    Add debugging

--- scripts/publish_package.py
+++ scripts/publish_package.py
@@ -7,8 +7,9 @@ only_version = sys.argv[1]
 print("Configuring git ...")
 subprocess.run(["git", "config", "user.email", os.environ['GIT_EMAIL']], check=True)
 subprocess.run(["git", "config", "--global", "user.email", os.environ['GIT_USERNAME']], check=True)
+out = subprocess.run(["git", "pull"], capture_output=True, check=True)
+print(out.stderr)
 subprocess.run(["git", "checkout", "main"], check=True)
-subprocess.run(["git", "pull"], check=True)
 
 print("Tagging package version ...")
 output = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True, check=True)
