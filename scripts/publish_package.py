c73a965193b8e3ce6c5a10ed1a014ad526f6a64c
Adam Davies <adam.davies@appex-development.com>
2022-09-07 10:00:56 +0100

    Fix publish package script

--- scripts/publish_package.py
+++ scripts/publish_package.py
@@ -11,16 +11,14 @@ subprocess.run(["git", "fetch"], check=True)
 subprocess.run(["git", "checkout", "main"], check=True)
 subprocess.run(["git", "pull"], check=True)
 
-# print("Tagging package version ...")
-# output = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True, check=True)
-# tag = output.stdout.decode("utf-8").replace("\n", "")
-# subprocess.run(["npm", "version", tag, "-m", f'"Release version: {tag}"'], check=True)
-# subprocess.run(["git", "push", "origin", "HEAD:main"], check=True)
-# print(f'Version {tag} tagged successfully')
-
-print(os.environ)
+print("Tagging package version ...")
+output = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True, check=True)
+tag = output.stdout.decode("utf-8").replace("\n", "")
+subprocess.run(["npm", "version", tag, "-m", f'"Release version: {tag}"'], check=True)
+subprocess.run(["git", "push", "origin", "HEAD:main"], check=True)
+print(f'Version {tag} tagged successfully')
 
 if only_version == "false":
     print("Publishing package")
-    subprocess.run(["npm", "publish"], check=True, env=os.environ.copy())
+    subprocess.run(["npm", "publish"], check=True)
     print("Package published successfully")
