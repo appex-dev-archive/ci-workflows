479a8ddb57b02a391c68539110a0a2607b88e03a
Adam Davies <adam.davies@appex-development.com>
2022-09-07 09:16:36 +0100

    Clean up

--- scripts/publish_package.py
+++ scripts/publish_package.py
@@ -7,9 +7,6 @@ only_version = sys.argv[1]
 print("Configuring git ...")
 subprocess.run(["git", "config", "--global", "user.email", os.environ['GIT_EMAIL']], check=True)
 subprocess.run(["git", "config", "--global", "user.name", os.environ['GIT_USERNAME']], check=True)
-# out = subprocess.run(["git", "pull"], capture_output=True)
-# print(out.stderr)
-# subprocess.run(["git", "checkout", "main"], check=True)
 
 print("Tagging package version ...")
 output = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True, check=True)
